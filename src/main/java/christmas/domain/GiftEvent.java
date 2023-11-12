d08e1862aaf3ae2f0a05599d23a408f8aa62ec30
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-11-13 00:51:21 +0900

    feat : GiftEvent 증정 이벤트 선물을 제공하는 기능 추가

--- src/main/java/christmas/domain/GiftEvent.java
+++ src/main/java/christmas/domain/GiftEvent.java
@@ -1,9 +1,18 @@
 package christmas.domain;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class GiftEvent {
     public static final int MIN_ORDER_AMOUNT_FOR_GIFT_EVENT = 12000;
 
     public boolean supports(long totalOrderAmount) {
         return totalOrderAmount >= MIN_ORDER_AMOUNT_FOR_GIFT_EVENT;
+    }
+
+    public Map<Menu, Integer> giveGift() {
+        Map<Menu, Integer> gifts = new HashMap<>();
+        gifts.put(Menu.CHAMPAGNE, 1);
+        return gifts;
     }
 }
--- src/test/java/christmas/domain/GiftEventTest.java
+++ src/test/java/christmas/domain/GiftEventTest.java
@@ -1,8 +1,12 @@
 package christmas.domain;
 
+import static christmas.domain.Menu.CHAMPAGNE;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -20,5 +24,20 @@ public class GiftEventTest {
 
         // then
         assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("증정 이벤트로 샴페인 1개를 제공한다.")
+    void give_champagne_for_gift_event() {
+        // given
+        GiftEvent giftEvent = new GiftEvent();
+
+        // when
+        Map<Menu, Integer> result = giftEvent.giveGift();
+
+        // then
+        assertThat(result.containsKey(CHAMPAGNE)).isTrue();
+        assertThat(result.get(CHAMPAGNE)).isEqualTo(1);
+        assertThat(result.size()).isEqualTo(1);
     }
 }
