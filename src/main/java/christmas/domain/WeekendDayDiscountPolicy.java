054a27758304096f105057590e75f919c8a42eb8
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-11-13 00:23:26 +0900

    feat : WeekendDayDiscountPolicy 주말 할인 금액 계산 기능 추가

--- src/main/java/christmas/domain/WeekendDayDiscountPolicy.java
+++ src/main/java/christmas/domain/WeekendDayDiscountPolicy.java
@@ -4,7 +4,13 @@ import java.time.DayOfWeek;
 
 public class WeekendDayDiscountPolicy {
 
+    public static final int DISCOUNT_PER_WEEKEND_MAIN_MENU = 2023;
+
     public boolean supports(DayOfWeek dayOfWeek) {
         return dayOfWeek == DayOfWeek.FRIDAY || dayOfWeek == DayOfWeek.SATURDAY;
+    }
+
+    public long calculateDiscountAmount(Menus menus) {
+        return menus.totalMainCount() * DISCOUNT_PER_WEEKEND_MAIN_MENU;
     }
 }
