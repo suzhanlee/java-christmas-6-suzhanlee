81cf4ab67b032155720aaabb97911f5e0df42f9f
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-11-13 00:24:46 +0900

    refactor : WeekDayDiscountPolicy 디저트 당 할인 금액 상수 이름 변경

--- src/main/java/christmas/domain/WeekDayDiscountPolicy.java
+++ src/main/java/christmas/domain/WeekDayDiscountPolicy.java
@@ -3,13 +3,13 @@ package christmas.domain;
 import java.time.DayOfWeek;
 
 public class WeekDayDiscountPolicy {
-    public static final int WEEK_DAY_DESSERT_PER_DISCOUNT_AMOUNT = 2023;
+    public static final int DISCOUNT_PER_WEEK_DAY_DESSERT = 2023;
 
     public boolean supports(DayOfWeek dayOfWeek) {
         return dayOfWeek != DayOfWeek.FRIDAY && dayOfWeek != DayOfWeek.SATURDAY;
     }
 
     public long calculateDiscountAmount(Menus menus) {
-        return menus.totalDessertCount() * WEEK_DAY_DESSERT_PER_DISCOUNT_AMOUNT;
+        return menus.totalDessertCount() * DISCOUNT_PER_WEEK_DAY_DESSERT;
     }
 }
