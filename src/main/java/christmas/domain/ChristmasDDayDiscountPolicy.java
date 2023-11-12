a19b1061e195271a00b3974c81806cde684c7bbe
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-11-12 16:51:15 +0900

    feat : ChristmasDDayDiscountPolicy 할인 금액 구하는 기능 추가

--- src/main/java/christmas/domain/ChristmasDDayDiscountPolicy.java
+++ src/main/java/christmas/domain/ChristmasDDayDiscountPolicy.java
@@ -1,7 +1,17 @@
 package christmas.domain;
 
 public class ChristmasDDayDiscountPolicy {
+
+    public static final int DEFAULT_CHRISTMAS_D_DAT_DISCOUNT_AMOUNT = 1000;
+    public static final int ONE_DAY = 1;
+    public static final int TWENTY_FIVE_DAY = 25;
+    public static final long CHRISTMAS_INCREMENT = 100L;
+
     public boolean supports(int day) {
-        return day >= 1 && day <= 25;
+        return day >= ONE_DAY && day <= TWENTY_FIVE_DAY;
+    }
+
+    public long calculateDiscountAmount(int day) {
+        return DEFAULT_CHRISTMAS_D_DAT_DISCOUNT_AMOUNT + (day - 1) * CHRISTMAS_INCREMENT;
     }
 }
--- src/test/java/christmas/domain/ChristmasDDayDiscountPolicyTest.java
+++ src/test/java/christmas/domain/ChristmasDDayDiscountPolicyTest.java
@@ -21,4 +21,18 @@ public class ChristmasDDayDiscountPolicyTest {
         // then
         assertThat(discountable).isEqualTo(expected);
     }
+
+    @ParameterizedTest
+    @DisplayName("할인 금액은 1000원으로 시작해 크리스마스가 다가올 수록 1000원씩 증가시킨다.")
+    @CsvSource(value = {"1, 1000", "2, 1100", "25, 3400"})
+    void discountAmount(int day, long expected) {
+        // given
+        ChristmasDDayDiscountPolicy christmasDDayDiscountPolicy = new ChristmasDDayDiscountPolicy();
+
+        // when
+        long discountAmount = christmasDDayDiscountPolicy.calculateDiscountAmount(day);
+
+        // then
+        assertThat(discountAmount).isEqualTo(expected);
+    }
 }
