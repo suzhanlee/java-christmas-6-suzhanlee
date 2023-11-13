59c048a4bbbc8f7ec618d8c44fc0e594c502349d
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-11-13 18:09:12 +0900

    refactor : Menu 메뉴 종류 확인 테스트 절차 간소화

--- src/test/java/christmas/domain/MenuTest.java
+++ src/test/java/christmas/domain/MenuTest.java
@@ -32,35 +32,35 @@ class MenuTest {
 
     @ParameterizedTest
     @DisplayName("메뉴의 종류가 음료인지 아닌지 알려준다.")
-    @CsvSource(value = {"레드와인, true", "양송이수프, false"})
-    void inform_menu_type_that_is_beverage_or_not(String menuName, boolean expected) {
-        // given
-        Menu menu = Menu.toMenu(menuName);
+    @CsvSource(value = {"RED_WINE, true", "MUSHROOM_CREAM_SOUP, false"})
+    void inform_menu_type_that_is_beverage_or_not(Menu menu, boolean expected) {
+        // when
+        boolean result = menu.isBeverage();
 
-        // when // then
-        assertThat(menu.isBeverage()).isEqualTo(expected);
+        // then
+        assertThat(result).isEqualTo(expected);
     }
 
     @ParameterizedTest
     @DisplayName("메뉴의 종류가 디저트인지 아닌지 알려준다.")
-    @CsvSource(value = {"아이스크림, true", "레드와인, false"})
-    void inform_menu_type_that_is_dessert_or_not(String menuName, boolean expected) {
-        // given
-        Menu menu = Menu.toMenu(menuName);
+    @CsvSource(value = {"ICE_CREAM, true", "RED_WINE, false"})
+    void inform_menu_type_that_is_dessert_or_not(Menu menu, boolean expected) {
+        // when
+        boolean result = menu.isDessert();
 
-        // when // then
-        assertThat(menu.isDessert()).isEqualTo(expected);
+        // then
+        assertThat(result).isEqualTo(expected);
     }
 
     @ParameterizedTest
     @DisplayName("메뉴의 종류가 메인 메뉴인지 아닌지 알려준다.")
-    @CsvSource(value = {"티본스테이크, true", "아이스크림, false"})
-    void inform_menu_type_that_is_main_or_not(String menuName, boolean expected) {
-        // given
-        Menu menu = Menu.toMenu(menuName);
+    @CsvSource(value = {"T_BONE_STEAK, true", "ICE_CREAM, false"})
+    void inform_menu_type_that_is_main_or_not(Menu menu, boolean expected) {
+        // when
+        boolean result = menu.isMain();
 
-        // when // then
-        assertThat(menu.isMain()).isEqualTo(expected);
+        // then
+        assertThat(result).isEqualTo(expected);
     }
 
     @ParameterizedTest
