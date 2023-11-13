fbf6c60a17ac2d6f7dd372d9f4ebc399dec3538e
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-11-13 16:35:28 +0900

    fix : ChristmasDDayDiscountPolicy DiscountPolicy 인터페이스에 맞게 선언부 수정

--- src/main/java/christmas/domain/ChristmasDDayDiscountPolicy.java
+++ src/main/java/christmas/domain/ChristmasDDayDiscountPolicy.java
@@ -1,17 +1,30 @@
 package christmas.domain;
 
-public class ChristmasDDayDiscountPolicy {
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ChristmasDDayDiscountPolicy implements DiscountPolicy {
 
     public static final int DEFAULT_CHRISTMAS_D_DAT_DISCOUNT_AMOUNT = 1000;
-    public static final int ONE_DAY = 1;
     public static final int TWENTY_FIVE_DAY = 25;
     public static final long CHRISTMAS_INCREMENT = 100L;
+    public static final String CHRISTMAS_D_DAY_DISCOUNT = "크리스마스 디데이 할인";
+    private final LocalDate localDate;
 
-    public boolean supports(int day) {
-        return day >= ONE_DAY && day <= TWENTY_FIVE_DAY;
+    public ChristmasDDayDiscountPolicy(LocalDate localDate) {
+        this.localDate = localDate;
     }
 
-    public long calculateDiscountAmount(int day) {
-        return DEFAULT_CHRISTMAS_D_DAT_DISCOUNT_AMOUNT + (day - 1) * CHRISTMAS_INCREMENT;
+    public boolean supports() {
+        return localDate.getDayOfMonth() <= TWENTY_FIVE_DAY;
+    }
+
+    public Map<String, Long> calculateDiscountAmount(Menus menus) {
+        Map<String, Long> benefitDetail = new HashMap<>();
+        long discountAmount =
+                DEFAULT_CHRISTMAS_D_DAT_DISCOUNT_AMOUNT + (localDate.getDayOfMonth() - 1) * CHRISTMAS_INCREMENT;
+        benefitDetail.put(CHRISTMAS_D_DAY_DISCOUNT, discountAmount);
+        return benefitDetail;
     }
 }
--- src/test/java/christmas/domain/ChristmasDDayDiscountPolicyTest.java
+++ src/test/java/christmas/domain/ChristmasDDayDiscountPolicyTest.java
@@ -1,7 +1,11 @@
 package christmas.domain;
 
+import static christmas.domain.ChristmasDDayDiscountPolicy.CHRISTMAS_D_DAY_DISCOUNT;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -13,10 +17,11 @@ public class ChristmasDDayDiscountPolicyTest {
     @CsvSource(value = {"1, true", "25, true", "26, false", "31, false"})
     void discount_condition(int day, boolean expected) {
         // given
-        ChristmasDDayDiscountPolicy christmasDDayDiscountPolicy = new ChristmasDDayDiscountPolicy();
+        LocalDate localDate = LocalDate.of(2023, 12, day);
+        ChristmasDDayDiscountPolicy christmasDDayDiscountPolicy = new ChristmasDDayDiscountPolicy(localDate);
 
         // when
-        boolean discountable = christmasDDayDiscountPolicy.supports(day);
+        boolean discountable = christmasDDayDiscountPolicy.supports();
 
         // then
         assertThat(discountable).isEqualTo(expected);
@@ -25,14 +30,22 @@ public class ChristmasDDayDiscountPolicyTest {
     @ParameterizedTest
     @DisplayName("할인 금액은 1000원으로 시작해 크리스마스가 다가올 수록 1000원씩 증가시킨다.")
     @CsvSource(value = {"1, 1000", "2, 1100", "25, 3400"})
-    void calculate_discount_amount(int day, long expected) {
+    void calculate_discount_amount(int day, long discountAmount) {
         // given
-        ChristmasDDayDiscountPolicy christmasDDayDiscountPolicy = new ChristmasDDayDiscountPolicy();
+        LocalDate localDate = LocalDate.of(2023, 12, day);
+        Menus menus = new Menus("티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1");
+        ChristmasDDayDiscountPolicy christmasDDayDiscountPolicy = new ChristmasDDayDiscountPolicy(localDate);
 
         // when
-        long discountAmount = christmasDDayDiscountPolicy.calculateDiscountAmount(day);
+        Map<String, Long> result = christmasDDayDiscountPolicy.calculateDiscountAmount(menus);
 
         // then
-        assertThat(discountAmount).isEqualTo(expected);
+        assertThat(result).isEqualTo(createExpected(discountAmount));
+    }
+
+    private Map<String, Long> createExpected(long discountAmount) {
+        Map<String, Long> expected = new HashMap<>();
+        expected.put(CHRISTMAS_D_DAY_DISCOUNT, discountAmount);
+        return expected;
     }
 }
