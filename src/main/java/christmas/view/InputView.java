a70ea858cae0d693ae9c935f84b66c1c76abcb96
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-11-13 23:20:41 +0900

    refactor : InputView 정확한 의미 전달을 위해 visitDate 메소드 이름 변경 및 값의 상수화

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -5,17 +5,19 @@ public class InputView {
     public static final String INPUT_VISIT_DATE_MENT = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
     public static final String INVALID_VISIT_DATE_EXCEPTION = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
     public static final String INPUT_MENU_AND_NUMBER_MENT = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+    public static final int MIN_VISIT_DAY_OF_MONTH = 1;
+    public static final int MAX_VISIT_DAY_OF_MONTH = 31;
     private final ConsoleService consoleService;
 
     public InputView(ConsoleService consoleService) {
         this.consoleService = consoleService;
     }
 
-    public int visitDate() {
+    public int visitDayOfMonth() {
         try {
             System.out.println(INPUT_VISIT_DATE_MENT);
             int visitDayOfMonth = Integer.parseInt(consoleService.readLine());
-            if (visitDayOfMonth < 1 || visitDayOfMonth >= 31) {
+            if (visitDayOfMonth < MIN_VISIT_DAY_OF_MONTH || visitDayOfMonth >= MAX_VISIT_DAY_OF_MONTH) {
                 throw new IllegalStateException(INVALID_VISIT_DATE_EXCEPTION);
             }
             return visitDayOfMonth;
