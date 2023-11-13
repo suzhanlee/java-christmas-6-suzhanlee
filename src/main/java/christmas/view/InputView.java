195862e45a9eb87118b2a334c3932275896cc30f
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-11-13 20:17:32 +0900

    feat : InputView 주문할 메뉴와 개수를 입력하는 기능 추가

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -4,6 +4,7 @@ public class InputView {
 
     public static final String INPUT_VISIT_DATE_MENT = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
     public static final String INVALID_VISIT_DATE_EXCEPTION = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+    public static final String INPUT_MENU_AND_NUMBER_MENT = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
     private final ConsoleService consoleService;
 
     public InputView(ConsoleService consoleService) {
@@ -21,5 +22,10 @@ public class InputView {
         } catch (NumberFormatException e) {
             throw new IllegalStateException(INVALID_VISIT_DATE_EXCEPTION);
         }
+    }
+
+    public String menuAndNumber() {
+        System.out.println(INPUT_MENU_AND_NUMBER_MENT);
+        return consoleService.readLine();
     }
 }
