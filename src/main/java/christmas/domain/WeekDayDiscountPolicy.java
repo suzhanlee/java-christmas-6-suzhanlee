6bfceae7585bf7fb6efac0ff9d80d97d5664aea8
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-11-13 16:47:37 +0900

    fix : WeekDayDiscountPolicy DiscountPolicy 인터페이스에 맞게 선언부 수정

--- src/main/java/christmas/domain/WeekDayDiscountPolicy.java
+++ src/main/java/christmas/domain/WeekDayDiscountPolicy.java
@@ -1,15 +1,28 @@
 package christmas.domain;
 
 import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 
-public class WeekDayDiscountPolicy {
+public class WeekDayDiscountPolicy implements DiscountPolicy {
     public static final int DISCOUNT_PER_WEEK_DAY_DESSERT = 2023;
+    public static final String WEEK_DAY_DISCOUNT = "평일 할인";
+    private final LocalDate localDate;
 
-    public boolean supports(DayOfWeek dayOfWeek) {
+    public WeekDayDiscountPolicy(LocalDate localDate) {
+        this.localDate = localDate;
+    }
+
+    public boolean supports() {
+        DayOfWeek dayOfWeek = localDate.getDayOfWeek();
         return dayOfWeek != DayOfWeek.FRIDAY && dayOfWeek != DayOfWeek.SATURDAY;
     }
 
-    public long calculateDiscountAmount(Menus menus) {
-        return menus.totalDessertCount() * DISCOUNT_PER_WEEK_DAY_DESSERT;
+    public Map<String, Long> calculateDiscountAmount(Menus menus) {
+        Map<String, Long> benefitDetail = new HashMap<>();
+        long discountAmount = menus.totalDessertCount() * DISCOUNT_PER_WEEK_DAY_DESSERT;
+        benefitDetail.put(WEEK_DAY_DISCOUNT, discountAmount);
+        return benefitDetail;
     }
 }
--- src/test/java/christmas/domain/WeekDayDiscountPolicyTest.java
+++ src/test/java/christmas/domain/WeekDayDiscountPolicyTest.java
@@ -1,8 +1,13 @@
 package christmas.domain;
 
+import static christmas.domain.WeekDayDiscountPolicy.WEEK_DAY_DISCOUNT;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -15,26 +20,37 @@ public class WeekDayDiscountPolicyTest {
     @CsvSource(value = {"SUNDAY, true", "THURSDAY, true", "FRIDAY, false", "SATURDAY, false"})
     void discount_condition(DayOfWeek dayOfWeek, boolean expected) {
         // given
-        WeekDayDiscountPolicy weekDayDiscountPolicy = new WeekDayDiscountPolicy();
+        WeekDayDiscountPolicy weekDayDiscountPolicy = new WeekDayDiscountPolicy(createLocalDateBy(dayOfWeek));
 
         // when
-        boolean result = weekDayDiscountPolicy.supports(dayOfWeek);
+        boolean result = weekDayDiscountPolicy.supports();
 
         // then
         assertThat(result).isEqualTo(expected);
+    }
+
+    private LocalDate createLocalDateBy(DayOfWeek dayOfWeek) {
+        return LocalDate.of(2023, 12, 13).with(TemporalAdjusters.nextOrSame(dayOfWeek));
     }
 
     @Test
     @DisplayName("디저트 메뉴를 메뉴 1개당 2023원 할인한다.")
     void calculate_discount_amount() {
         // given
-        WeekDayDiscountPolicy weekDayDiscountPolicy = new WeekDayDiscountPolicy();
+        LocalDate localDate = LocalDate.of(2023, 12, 12);
+        WeekDayDiscountPolicy weekDayDiscountPolicy = new WeekDayDiscountPolicy(localDate);
         Menus menus = new Menus("티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1");
 
         // when
-        long result = weekDayDiscountPolicy.calculateDiscountAmount(menus);
+        Map<String, Long> result = weekDayDiscountPolicy.calculateDiscountAmount(menus);
 
         // then
-        assertThat(result).isEqualTo(4046);
+        assertThat(result).isEqualTo(createExpected());
+    }
+
+    private Map<String, Long> createExpected() {
+        Map<String, Long> expected = new HashMap<>();
+        expected.put(WEEK_DAY_DISCOUNT, 4046L);
+        return expected;
     }
 }
