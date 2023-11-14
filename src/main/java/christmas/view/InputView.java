46adb3aeb662c5d32a310393a326721562e9bd90
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-11-14 19:26:11 +0900

    refactor : InputView 출력 메시지의 변수 명을 MENT -> MESSAGE 로 변경

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -2,9 +2,9 @@ package christmas.view;
 
 public class InputView {
 
-    public static final String INPUT_VISIT_DATE_MENT = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    public static final String INPUT_VISIT_DATE_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
     public static final String INVALID_VISIT_DATE_EXCEPTION = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
-    public static final String INPUT_MENU_AND_NUMBER_MENT = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+    public static final String INPUT_MENU_AND_NUMBER_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
     public static final int MIN_VISIT_DAY_OF_MONTH = 1;
     public static final int MAX_VISIT_DAY_OF_MONTH = 31;
     private final ConsoleService consoleService;
@@ -15,7 +15,7 @@ public class InputView {
 
     public int visitDayOfMonth() {
         try {
-            System.out.println(INPUT_VISIT_DATE_MENT);
+            System.out.println(INPUT_VISIT_DATE_MESSAGE);
             int visitDayOfMonth = getVisitDayOfMonth();
             validateDayOfMonth(visitDayOfMonth);
             return visitDayOfMonth;
@@ -39,7 +39,7 @@ public class InputView {
     }
 
     public String menuAndNumber() {
-        System.out.println(INPUT_MENU_AND_NUMBER_MENT);
+        System.out.println(INPUT_MENU_AND_NUMBER_MESSAGE);
         return consoleService.readLine();
     }
 }
