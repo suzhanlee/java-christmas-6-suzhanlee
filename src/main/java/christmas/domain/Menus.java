d20083f9b40bbcabf0f319be9289c19e06986192
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-11-15 16:53:03 +0900

    refactor : Menus 메뉴 갯수에 대한 의미 전달을 명확히 하기 위해 이름을 number에서 quantity로 변경

--- src/main/java/christmas/domain/Menus.java
+++ src/main/java/christmas/domain/Menus.java
@@ -12,9 +12,9 @@ import java.util.regex.Pattern;
 public class Menus {
     public static final String MENU_REGEX = "([가-힣]+-\\d+,?)+";
     public static final String DASH = "-";
-    public static final int DEFAULT_MENU_NUMBER_VALUE = 0;
+    public static final int DEFAULT_MENU_QUANTITY_VALUE = 0;
     public static final int MENU_INDEX = 0;
-    public static final int MENU_NUMBER_INDEX = 1;
+    public static final int MENU_QUANTITY_INDEX = 1;
     public static final String COMMA = ",";
     private final Map<Menu, Integer> menus;
 
@@ -27,8 +27,8 @@ public class Menus {
         List<String> overlappedMenus = split(menuForm);
         Map<Menu, Integer> uniqueMenus = createUniqueMenus(overlappedMenus);
         validateDuplication(uniqueMenus, overlappedMenus);
-        validateMenusCount(uniqueMenus);
-        validateMenuTotalMenuCount(uniqueMenus);
+        validateMenusQuantity(uniqueMenus);
+        validateMenuTotalMenuQuantity(uniqueMenus);
         validateOrderedMenuType(uniqueMenus);
         return uniqueMenus;
     }
@@ -51,24 +51,24 @@ public class Menus {
         return List.of(menuForm.split(COMMA));
     }
 
-    private Map<Menu, Integer> createUniqueMenus(List<String> menusAndNumbers) {
-        Map<Menu, Integer> menus = new HashMap<>();
-        menusAndNumbers.forEach(menuAndNumber -> addMenuAndNumber(separateMenuAndNumber(menuAndNumber), menus));
-        return menus;
+    private Map<Menu, Integer> createUniqueMenus(List<String> menusAndQuantity) {
+        Map<Menu, Integer> uniqueMenus = new HashMap<>();
+        menusAndQuantity.forEach(menuAndQuantity -> addMenuAndQuantity(separateMenuAndQuantity(menuAndQuantity), uniqueMenus));
+        return uniqueMenus;
     }
 
-    private void addMenuAndNumber(List<String> seperatedMenuAndNumber, Map<Menu, Integer> menuMap) {
-        Menu menu = Menu.toMenu(seperatedMenuAndNumber.get(MENU_INDEX));
-        int menuNumber = Integer.parseInt(seperatedMenuAndNumber.get(MENU_NUMBER_INDEX));
-        menuMap.put(menu, menuMap.getOrDefault(menu, DEFAULT_MENU_NUMBER_VALUE) + menuNumber);
+    private void addMenuAndQuantity(List<String> seperatedMenuAndQuantity, Map<Menu, Integer> uniqueMenus) {
+        Menu menu = Menu.toMenu(seperatedMenuAndQuantity.get(MENU_INDEX));
+        int quantity = Integer.parseInt(seperatedMenuAndQuantity.get(MENU_QUANTITY_INDEX));
+        uniqueMenus.put(menu, uniqueMenus.getOrDefault(menu, DEFAULT_MENU_QUANTITY_VALUE) + quantity);
     }
 
-    private List<String> separateMenuAndNumber(String menuAndNumber) {
-        return List.of(menuAndNumber.split(DASH));
+    private List<String> separateMenuAndQuantity(String menuAndQuantity) {
+        return List.of(menuAndQuantity.split(DASH));
     }
 
-    private void validateDuplication(Map<Menu, Integer> menus, List<String> menusAndNumbers) {
-        if (isSame(menus.size(), menusAndNumbers.size())) {
+    private void validateDuplication(Map<Menu, Integer> menus, List<String> menusAndQuantity) {
+        if (isSame(menus.size(), menusAndQuantity.size())) {
             throw new InputMenuException();
         }
     }
@@ -77,32 +77,32 @@ public class Menus {
         return actualMenuSize != expectedMenuSize;
     }
 
-    private void validateMenusCount(Map<Menu, Integer> uniqueMenus) {
-        uniqueMenus.values().forEach(this::validateMenuCount);
+    private void validateMenusQuantity(Map<Menu, Integer> uniqueMenus) {
+        uniqueMenus.values().forEach(this::validateMenuQuantity);
     }
 
-    private void validateMenuCount(Integer menuCount) {
-        if (!existMenuOrder(menuCount)) {
+    private void validateMenuQuantity(int quantity) {
+        if (!existMenuOrder(quantity)) {
             throw new InputMenuException();
         }
     }
 
-    private boolean existMenuOrder(Integer menuCount) {
-        return menuCount >= 1;
+    private boolean existMenuOrder(int quantity) {
+        return quantity >= 1;
     }
 
-    private void validateMenuTotalMenuCount(Map<Menu, Integer> uniqueMenus) {
+    private void validateMenuTotalMenuQuantity(Map<Menu, Integer> uniqueMenus) {
         if (greaterThanTwenty(uniqueMenus)) {
             throw new TotalMenuNumberLimitException();
         }
     }
 
     private boolean greaterThanTwenty(Map<Menu, Integer> uniqueMenus) {
-        return getTotalMenuCount(uniqueMenus) > 20;
+        return getTotalMenuQuantity(uniqueMenus) > 20;
     }
 
-    private int getTotalMenuCount(Map<Menu, Integer> uniqueMenus) {
-        return uniqueMenus.values().stream().mapToInt(menuCount -> menuCount).sum();
+    private int getTotalMenuQuantity(Map<Menu, Integer> uniqueMenus) {
+        return uniqueMenus.values().stream().mapToInt(menuQuantity -> menuQuantity).sum();
     }
 
     private void validateOrderedMenuType(Map<Menu, Integer> uniqueMenus) {
@@ -140,9 +140,9 @@ public class Menus {
         return menus.entrySet().stream().mapToLong(this::calculateTotalMenuPrice).sum();
     }
 
-    private long calculateTotalMenuPrice(Entry<Menu, Integer> menuAndNumber) {
-        Menu menu = menuAndNumber.getKey();
-        return menu.totalMenuPrice(menuAndNumber.getValue());
+    private long calculateTotalMenuPrice(Entry<Menu, Integer> menuAndQuantity) {
+        Menu menu = menuAndQuantity.getKey();
+        return menu.totalMenuPrice(menuAndQuantity.getValue());
     }
 
     public Map<Menu, Integer> getMenus() {
--- src/test/java/christmas/domain/MenusTest.java
+++ src/test/java/christmas/domain/MenusTest.java
@@ -24,10 +24,10 @@ public class MenusTest {
     @DisplayName("메뉴 형식이 <메뉴-메뉴 개수,메뉴-메뉴 개수, ...> 와 다른 경우 예외를 던진다.")
     void menu_format_exception() {
         // given
-        String menuNamesAndNumbers = "타파스-1,제로콜라-1-";
+        String menuNamesAndQuantity = "타파스-1,제로콜라-1-";
 
         // when // then
-        assertThatThrownBy(() -> new Menus(menuNamesAndNumbers))
+        assertThatThrownBy(() -> new Menus(menuNamesAndQuantity))
                 .isExactlyInstanceOf(InputMenuException.class);
     }
 
@@ -35,46 +35,46 @@ public class MenusTest {
     @DisplayName("메뉴 형식은 <메뉴-메뉴 개수,메뉴-메뉴 개수, ...>와 같아야 한다.")
     void normal_menu_format() {
         // given
-        String menuNamesAndNumbers = "타파스-1,제로콜라-1";
+        String menuNamesAndQuantity = "타파스-1,제로콜라-1";
 
         // when // then
-        assertThatCode(() -> new Menus(menuNamesAndNumbers))
+        assertThatCode(() -> new Menus(menuNamesAndQuantity))
                 .doesNotThrowAnyException();
     }
 
     @ParameterizedTest
     @DisplayName("중복 메뉴를 입력한 경우 예외를 던진다.")
     @ValueSource(strings = {"타파스-1,제로콜라-1,타파스-1", "제로콜라-1,타파스-1,제로콜라-3"})
-    void validate_duplicate_menu(String menuNamesAndNumbers) {
+    void validate_duplicate_menu(String menuNamesAndQuantity) {
         // when // then
-        assertThatThrownBy(() -> new Menus(menuNamesAndNumbers))
+        assertThatThrownBy(() -> new Menus(menuNamesAndQuantity))
                 .isExactlyInstanceOf(InputMenuException.class);
     }
 
     @ParameterizedTest
     @DisplayName("메뉴가 1개 미만인 경우 예외를 던진다.")
     @ValueSource(strings = {"타파스-0,제로콜라-1", "제로콜라-1,타파스-0", "양송이수프-0"})
-    void validate_menu_count(String menuNamesAndNumbers) {
+    void validate_menu_quantity(String menuNamesAndQuantity) {
         // when // then
-        assertThatThrownBy(() -> new Menus(menuNamesAndNumbers))
+        assertThatThrownBy(() -> new Menus(menuNamesAndQuantity))
                 .isExactlyInstanceOf(InputMenuException.class);
     }
 
     @ParameterizedTest
     @DisplayName("총 주문 메뉴가 20개 초과면 예외를 던진다.")
     @ValueSource(strings = {"타파스-20,제로콜라-1", "제로콜라-2,타파스-20", "양송이수프-21"})
-    void validate_total_menu_of_orders(String menuNamesAndNumbers) {
+    void validate_total_menu_quantity_of_orders(String menuNamesAndQuantity) {
         // when // then
-        assertThatThrownBy(() -> new Menus(menuNamesAndNumbers))
+        assertThatThrownBy(() -> new Menus(menuNamesAndQuantity))
                 .isExactlyInstanceOf(TotalMenuNumberLimitException.class);
     }
 
     @ParameterizedTest
     @DisplayName("음료만 주문시 예외를 던진다.")
     @ValueSource(strings = {"제로콜라-3", "제로콜라-2,레드와인-10", "샴페인-1"})
-    void validate_menu_type(String menuNamesAndNumbers) {
+    void validate_menu_type(String menuNamesAndQuantity) {
         // when // then
-        assertThatThrownBy(() -> new Menus(menuNamesAndNumbers))
+        assertThatThrownBy(() -> new Menus(menuNamesAndQuantity))
                 .isExactlyInstanceOf(OrderMenuTypeException.class);
     }
 
